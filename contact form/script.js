96ae5f345428465130062c18df8b36582e4d26b2
arpit thakur <arpitthakut722@gmail.com>
2022-07-04 13:41:37 +0530

    updated contact form with table data

--- contact form/script.js
+++ contact form/script.js
@@ -1,13 +1,57 @@
 // var firstName = document.getElementById("fname").value;
-
-
+var contacts = [];
 function submitForm(){
- let firstName = document.getElementById("fname").value;
+  validation ();
+
+ let fname = document.getElementById("fname").value;
+ let email = document.getElementById("email").value;
+ let phNumber= document.getElementById("phNumber").value;
+ 
+  var obj = {};
+  obj["fname"] = fname;
+  obj["email"] = email;
+  obj["phNumber"] = phNumber;
+  
+ 
+  contacts.push(obj);
+  
+  
+
+    let table = document.getElementById("table");
+    table.innerHTML = '';
+  
+  for (let i = 0; i < contacts.length; i++) {
+ 
+      
+      let row = table.insertRow();
+      let td1 = row.insertCell(0);
+      let td2 = row.insertCell(1);
+      let td3 = row.insertCell(2);
+ 
+      td1.innerHTML = contacts[i].fname;
+      td2.innerHTML = contacts[i].email;
+      td3.innerHTML = contacts[i].phNumber;
+  }
+
+
+     
+  document.getElementById("fname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phNumber").value = "";
+ 
+
+
+  return false;
+}
+
+
+function validation(){
+  let fname = document.getElementById("fname").value;
  let email = document.getElementById("email").value;
  let phNumber= document.getElementById("phNumber").value;
 
 
-  if(firstName == "" ){
+ if(fname == "" ){
    
     document.getElementById("alert").style.display = 'block';
     return false;
@@ -30,9 +74,4 @@ function submitForm(){
     document.getElementById("alert3").style.display='none';
   }
 
-  document.getElementById("fname").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("phNumber").value = "";
-  return false;
- 
 }
